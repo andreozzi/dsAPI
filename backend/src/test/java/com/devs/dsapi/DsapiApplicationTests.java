ebdec5948acd479472d26774758a2331c72728c1
Victor Andreozzi <victor.andreozzi@gmail.com>
2022-11-17 02:41:21 -0300

    Project Created

---
+++ backend/src/test/java/com/devs/dsapi/DsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.dsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
