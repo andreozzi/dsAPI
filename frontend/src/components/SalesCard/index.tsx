288e7b34c72af3471e1a8b8e0a3b61310fe57a21
Victor Andreozzi <victor.andreozzi@gmail.com>
2022-11-19 22:44:12 -0300

    Send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -21,7 +21,6 @@ function SalesCard() {
         const dmax = maxDate.toISOString().slice(0, 10);
 
 
-        console.log(dmin);
 
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
@@ -78,7 +77,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="ds-red-button-conteiner">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id} />
                                         </div>
                                     </td>
                                 </tr>
